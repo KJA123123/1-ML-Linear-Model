8e7c922abd5ca13f9561f24c685b71d1a116502b
kja123123 <kayleighalexander05@gmail.com>
2025-04-29 10:31:41 +0000

    Git Setup

---
+++ Github_Setup.R
@@ -0,0 +1,10 @@
+# Required packages
+
+library(usethis)
+library(gitcreds)
+
+use_git_config(user.name  = "kja123123",
+               user.email = "kayleighalexander05@gmail.com")
+
+usethis::create_github_token()
+
